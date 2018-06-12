8022845dd3ca88f06b75393fd6c1012cc2bbc737
Anders Bakken <agbakken@gmail.com>
2018-06-12 09:54:05 -0700

    stuff

--- example/example.js
+++ example/example.js
@@ -3,23 +3,27 @@
 const lockfile = require('../lib/lockfile-pid');
 const fs = require('fs');
 
-var count = 10;
-function go() {
-    lockfile.lock("/tmp/lockfile-pid_test", {wait: 100}).then((unlock) => {
-        console.log(count);
-        fs.writeFileSync("/tmp/lockfile-pid_test.data", count--);
-        setTimeout(() => {
-            unlock();
-            if (!count)
-                process.exit();
-            setTimeout(go, 500);
-        }, 500);
-    }, (err) => {
-        console.log("Couldn't lock", err);
-        setTimeout(go, 1000);
-    });
-};
-go();
+// var count = 100;
+// function go() {
+//     lockfile.lock("/tmp/lockfile-pid_test", {wait: 100}).then((unlock) => {
+//         console.log(count);
+//         fs.writeFileSync("/tmp/lockfile-pid_test.data", count--);
+//         setTimeout(() => {
+//             unlock();
+//             if (!count)
+//                 process.exit();
+//             setTimeout(go, 500);
+//         }, 500);
+//     }, (err) => {
+//         console.log("Couldn't lock", err);
+//         setTimeout(go, 1000);
+//     });
+// };
+// go();
+lockfile.lock("/tmp/lockfile-crap", {wait: 100}).then((unlock) => {
+    console.log("got lock");
+    setTimeout(unlock, 100000);
+});
 
 process.on('unhandledRejection', (reason, p) => {
     console.error(reason, 'Unhandled Rejection at Promise', p);
